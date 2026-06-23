5bb78b30204511a484c13281feee61f7ce0ff45b
pbouin <philippe.bouin@fiabila.com>
2026-06-23 11:26:19 +0200

    test chhangement

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const greeting = document.getElementById("greeting");
 const cta = document.getElementById("cta");
 
 function sayHello(name) {
-  return `Hello, ${name}!`;
+  return `Hello world`;
 }
 
 cta.addEventListener("click", () => {
