3900d3b1916e5fc39ef676fd78c58f754d917cd1
Gabriel Cabon <gabriel.cabon@fiabila.com>
2026-06-23 11:40:18 +0200

    text

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const greeting = document.getElementById("greeting");
 const cta = document.getElementById("cta");
 
 function sayHello(name) {
-  return `Good morning ${name}!`;
+  return `Good morning Child ${name}!`;
 }
-
 cta.addEventListener("click", () => {
   greeting.textContent = sayHello("world");
 });
